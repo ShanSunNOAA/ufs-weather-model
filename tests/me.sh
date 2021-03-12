2161ce3d618f55f217569a485dbcd5de6ab44d45
Shan.Sun <shan.sun@noaa.gov>
2021-03-12 05:05:47 +0000

    Merge to FV3 top

--- tests/me.sh
+++ tests/me.sh
@@ -625,7 +625,7 @@ EOF
     elif [[ $ECFLOW == true ]]; then
       ecflow_create_compile_task
     else
-      ./compile.sh $MACHINE_ID "${MAKE_OPT}" $COMPILE_NR NO NO > ${LOG_DIR}/compile_${COMPILE_NR}.log 2>&1
+      ./compile.sh $MACHINE_ID "${MAKE_OPT}" $COMPILE_NR YES NO > ${LOG_DIR}/compile_${COMPILE_NR}.log 2>&1
     fi
 
     # Set RT_SUFFIX (regression test run directories and log files) and BL_SUFFIX
